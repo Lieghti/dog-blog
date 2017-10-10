075f8a5b62530da0a578f126bb24cb1aebf8fc33
lieghti sharp <lieghti@gmail.com>
2017-10-09 20:53:24 -0700

    changed some things around.

---
+++ js/.scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $("h1").click(function(){
+    alert("This is a header.");
+  });
+
+  //button stuff
+  $("button#gray").click(function(){
+    $("body").removeClass();
+    });
+
+    $("button#green").click(function(){
+      alert("This button works!");
+      // $("body").addClass("forest-background");
+      });
+
+
+
+
+
+});
